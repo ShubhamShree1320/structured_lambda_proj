89856b2bfc6b5c70334ff2585cc7487e227bfbe7
ShubhamShree1320 <shubham.ghostriderdown@gmail.com>
2024-09-09 10:51:14 +0530

    Made a linting adjustment
    This change doesnot change the functionality

--- modules/decoding_jwt.py
+++ modules/decoding_jwt.py
@@ -1,10 +1,13 @@
 from dependencies import jwt
-def decoding_with_jwt(token,secret_key):
+
+
+def decoding_with_jwt(token, secret_key):
     try:
         # Decode the JWT
-        decoded_payload = jwt.decode(token, secret_key, algorithms=['HS256'])
-        return (f"Decoded Payload: {decoded_payload}")
+        decoded_payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+        return f"Decoded Payload: {decoded_payload}"
     except jwt.ExpiredSignatureError:
         print("Token has expired.")
     except jwt.InvalidTokenError:
         print("Invalid token.")
+
